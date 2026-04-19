8804a0a972f462252707649fa881576f772a1a7b
Mehmet Karagöz <eng.mkaragoz@gmail.com>
2026-04-19 11:44:12 +0100

    Update minimum iOS platform to 13.0

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "AMRAdapterIronsource",
     platforms: [
-        .iOS(.v12)
+        .iOS(.v13)
     ],
     products: [
         .library(
